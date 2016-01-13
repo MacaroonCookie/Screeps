2daaaf3324b4467bab9eb9f68144d3488023f99f
Seth Cook <seth.cook@purdue.edu>
2016-01-13 13:01:49 -0500

    Adding spawn handler.

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
 var harvester = require('harvester');
+var spawn_handler = require('spawn');
 
 module.exports.loop = function () {
-    var spawn = Game.spawns.Galactica;
+  for( var sName in Game.spawns ) {
+    spawn_handler(Game.spawns[sName]);
+  }
 
-    /*if( Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], 'Worker' + (Object.keys(Game.creeps).length + 1)) != ERR_NOT_ENOUGH_ENERGY ) {
-        console.log('Created Worker' + Object.keys(Game.creeps).length );
-    }*/
-
-    for( var cName in Game.creeps ) {
-        var creep = Game.creeps[cName];
-        if( creep.memory.role == 'harvester') { harvester(creep); }
-        else if( creep.memory.role == 'builder' ) { 
-            if( creep.carry.energy == 0 ) {
-                if( Game.spawns.Galactica.transferEnergy(creep) == ERR_NOT_IN_RANGE ) {
-                    creep.moveTo(Game.spawns.Galactica);
-                }
-            } else {
-                var sites = creep.room.find(FIND_CONSTRUCTION_SITES);
-                if( sites.length ) {
-                    if( creep.build(sites[0]) == ERR_NOT_IN_RANGE ) {
-                        creep.moveTo(sites[0]);
-                    }
-                }
-            }
-        } else if(creep.memory.role == 'guard') {
-	        var targets = creep.room.find(FIND_HOSTILE_CREEPS);
-	        if(targets.length) {
-		        if(creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
-			        creep.moveTo(targets[0]);		
-		        }
-	        }
-        } else if(creep.memory.role == 'upgrader') {
-            if( creep.carry.energy == 0 ) {
-                if( Game.spawns.Galactica.transferEnergy(creep) == ERR_NOT_IN_RANGE ) {
-                    creep.moveTo(Game.spawns.Galactica);
-                }
-            } else {
-                if( creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE ) {
-                    creep.moveTo(creep.room.controller);
-                }
-            }
+  for( var cName in Game.creeps ) {
+      var creep = Game.creeps[cName];
+      if( creep.memory.role == 'harvester') { harvester(creep); }
+      else if( creep.memory.role == 'builder' ) { 
+          if( creep.carry.energy == 0 ) {
+              if( Game.spawns.Galactica.transferEnergy(creep) == ERR_NOT_IN_RANGE ) {
+                  creep.moveTo(Game.spawns.Galactica);
+              }
+          } else {
+              var sites = creep.room.find(FIND_CONSTRUCTION_SITES);
+              if( sites.length ) {
+                  if( creep.build(sites[0]) == ERR_NOT_IN_RANGE ) {
+                      creep.moveTo(sites[0]);
+                  }
+              }
+          }
+      } else if(creep.memory.role == 'guard') {
+        var targets = creep.room.find(FIND_HOSTILE_CREEPS);
+        if(targets.length) {
+          if(creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets[0]);		
+          }
         }
-    }
+      } else if(creep.memory.role == 'upgrader') {
+          if( creep.carry.energy == 0 ) {
+              if( Game.spawns.Galactica.transferEnergy(creep) == ERR_NOT_IN_RANGE ) {
+                  creep.moveTo(Game.spawns.Galactica);
+              }
+          } else {
+              if( creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE ) {
+                  creep.moveTo(creep.room.controller);
+              }
+          }
+      }
+  }
 }
---
+++ spawn.js
@@ -0,0 +1,41 @@
+module.export = function(spawn) {
+
+
+  if( typeof spawn.memory.creep_roles == 'undefined' ) {
+    spawn.memory.creep_roles = [];
+    spawn.memory.creep_next_id = 0;
+  } else {
+    creep_roles = spawn.memory.creep_roles;
+
+    for( var role in creep_roles ) {
+      // Correct malformed creeper roles
+      if( role.indexOf('body') == -1 ) { role['body'] = []; }
+      if( role.indexOf('creepName') == -1 ) { role['creepName'] = null; }
+      if( role.indexOf('memory') == -1 ) { role['memory'] = {role: 'NaN'}; }
+
+      // Check creep exists, if not
+      if( role['creepName'] == null or typeof Game.creeps[role['creepName']] == 'undefined' ) {
+        role['creepName'] = spawn.name + spawn.memory.creep_next_id.toString(16);
+        var result = spawn.createCreep(role['body'], role['creepName'], role['memory');
+
+        if( typeof result == 'string' ) {
+          spawn.memory.creep_next_id += 1;
+        } else {
+          switch(result) {
+            case ERR_INVALID_ARGS:
+                console.log('ERROR: Invalid arguments in spawn creeper auto-generation.');
+                role['creepName'] = null;
+                break;
+            case ERR_NAME_EXISTS:
+                spawn.memory.creep_next_id += 1;
+            case ERR_BUSY:
+            case ERR_NOT_ENOUGH_ENERGY:
+            default:
+                role['creepName'] = null;
+                continue;
+          }
+        }
+      }
+    }
+  }
+}
