7d5d6eea6fe37e03120ed21e23c755675c7ee1b1
Seth Cook <sethcook@purdue.edu>
2017-01-03 23:27:58 -0500

    Added transfer task.

---
+++ screeps/tasks/transfer.js
@@ -0,0 +1,47 @@
+var transfer = {
+  name: 'transfer',
+  memory_structure: {
+    targetId: null,
+    resource: 'any',
+    amount: null
+  },
+  run: function(creep) {
+    if( creep.memory.task.targetId == null ) {
+      return TASK_FAILED;
+    }
+
+    var target = Game.getObjectById(creep.memory.task.targetId);
+    if( target == null ) {
+      return TASK_FAILED;
+    }
+
+    if( ! creep.isNearTo(target) ) {
+      return TASK_FAILED;
+    }
+
+    var transferred = false;
+    for(var resource in creep.carry) {
+      if( resource == creep.memory.task.resource || creep.memory.task.resource == 'any' ) {
+        var success = null;
+        if( creep.memory.amount == null ) {
+          success = creep.transfer(target, resource);
+        } else {
+          success = creep.transfer(target, resource, creep.memory.task.amount);
+        }
+
+        if( ! (success == OK || success == ERR_FULL || success == ERR_INVALID_TARGET) ) {
+          return TASK_FAILED;
+        } else {
+          transferred = true;
+        }
+      }
+    }
+
+    if( ! transferred ) {
+      return TASK_FAILED;
+    }
+
+    return TASK_COMPLETED;
+  }
+};
+
