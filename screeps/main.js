f0b5896f9f3ef5fc41109bb2c908ed234dc7843d
Seth Cook <cook71@purdue.edu>
2017-05-18 18:34:02 -0400

    Added Spawn logic to created harvesters

--- screeps/main.js
+++ screeps/main.js
@@ -1,9 +1,18 @@
 module.exports.loop = function() {
+
+  for(var spawn in Game.spawns ) {
+    try {
+      spawn_handle.run(Game.spawns[spawn]);
+    } catch(err) {
+      console.log('[SPAWN=' + Game.spawns[spawn].name + '][ERROR] ' + err.stack);
+    }
+  }
+
   for(var creep in Game.creeps ) {
     try {
       creep_handle.run(Game.creeps[creep]);
     } catch(err) {
-      console.log('[CREEP=' + creep.name + '][ERROR] ' + err.stack);
+      console.log('[CREEP=' + Game.creeps[creep].name + '][ERROR] ' + err.stack);
     }
   }
 }
--- screeps/spawn.js
+++ screeps/spawn.js
@@ -1,51 +1,60 @@
-var spawn_handler = function(spawn) {
-  if( typeof spawn.memory.creep_next_id == 'undefined' ) {
-    spawn.memory.creep_next_id = 0;
-  }
+var spawn_handle = {
+  get_usable_storage_capacity: function(spawn) {
+    var storage_capacity = 0;
+    var structures = spawn.room.find(FIND_MY_STRUCTURES, {'filter': function(obj) { return _.contains([STRUCTURE_SPAWN,STRUCTURE_EXTENSION],obj.structureType);}});
 
-  if( typeof spawn.memory.creep_roles == 'undefined' ) {
-    spawn.memory.creep_roles = [];
-  } else {
-    creep_roles = spawn.memory.creep_roles;
+    for( var i=0; i<structures.length; i++ ) {
+      storage_capacity += structures[i].energyCapacity;
+    }
+    return storage_capacity;
+  },
 
-    for( var index in creep_roles ) {
-      var role = creep_roles[index];
+  get_number_parts: function(part, energy, weight) {
+    var parts = Math.floor( ((energy / BODYPART_COST[part]) * weight) / 100.0 );
+    return parts;
+  },
 
-      if( typeof role == 'string' ) {
-        console.log('Spawn ' + spawn.name + ': Creep Role #' + index.toString() + 'is a string, not object. Please re-add programmatically through the console.');
+  get_part_list: function(part_hash) {
+    var part_list = [];
+    for(var part in part_hash) {
+      for(var i=0; i<part_hash[part]; i++) {
+        part_list.push(part);
       }
+    }
+    return part_list;
+  },
 
-      // Correct malformed creeper roles
-      if( typeof role.body == 'undefined' ) { role['body'] = []; }
-      if( typeof role.creepName == 'undefined' ) { role['creepName'] = null; }
-      if( typeof role.memory == 'undefined' ) { role['memory'] = {role: 'NaN'}; }
+  generate_harvest_creep: function(spawn) {
+    var usable_energy = spawn_handle.get_usable_storage_capacity(spawn);
+    var carry_parts = spawn_handle.get_number_parts(CARRY, usable_energy, 40);
+    var move_parts  = spawn_handle.get_number_parts(MOVE,  usable_energy, 20);
+    var work_parts  = spawn_handle.get_number_parts(WORK,  usable_energy, 40);
+    var part_list = spawn_handle.get_part_list({carry: carry_parts, move: move_parts, work: work_parts});
+    if( spawn.canCreateCreep(part_list) == OK ) {
+      console.log('{move: '+move_parts +', carry: ' + carry_parts + ', work: ' + work_parts +'}');
+      var result = spawn.createCreep(part_list,{'roleName':'harvester'});
+    }
+  },
 
-      // Check creep exists, if not
-      if( role['creepName'] == null || typeof Game.creeps[role['creepName']] == 'undefined' ) {
-        role['creepName'] = spawn.name + '-' + spawn.memory.creep_next_id.toString(32);
-
-        var result = spawn.createCreep(role['body'], role['creepName'], role['memory']);
-
-        if( typeof result == 'string' ) {
-          spawn.memory.creep_next_id += 1;
-          console.log('Spawn ' + spawn.name + ': Created creep ' + role['creepName'] + ' for Role #' + index + '.');
-          break;
-        } else {
-          switch(result) {
-            case ERR_INVALID_ARGS:
-                console.log('ERROR: Invalid arguments in spawn creeper auto-generation.');
-                role['creepName'] = null;
-                break;
-            case ERR_NAME_EXISTS:
-                spawn.memory.creep_next_id += 1;
-            case ERR_BUSY:
-            case ERR_NOT_ENOUGH_ENERGY:
-            default:
-                role['creepName'] = null;
-                continue;
-          }
-        }
+  run: function(spawn) {
+    // Determine Harvesters to build
+    var harvest_capability = 0;
+    for( var creep in Game.creeps ) {
+      var c = Game.creeps[creep];
+      if( c.memory['roleName'] == 'harvester' ) {
+        harvest_capability = harvest_capability + (c.getActiveBodyparts(WORK)*2);
       }
+    }
+
+    var room_energy_capability = 0;
+    var sources = spawn.room.find(FIND_SOURCES);
+    for( var i=0; i<sources.length; i++ ) {
+      room_energy_capability += (sources[i].energyCapacity/300);
+    }
+
+    console.log(room_energy_capability);
+    if( (harvest_capability) < room_energy_capability && ! spawn.spawning) {
+      spawn_handle.generate_harvest_creep(spawn);
     }
   }
 }
